25395c748fd9a9f871a5d00185f7d2db2649eba3
Chris Adams <wave@chrisadams.me.uk>
2016-12-06 23:27:59 +0100

    Junk the markdownfield, in favour of streamfield

--- home/models.py
+++ home/models.py
@@ -3,19 +3,32 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+
+
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 
 from wagtailmarkdown.fields import MarkdownField, MarkdownPanel
 
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailembeds.blocks import EmbedBlock
 class HomePage(Page):
     pass
 
 class BlogPost(Page):
     date = models.DateField("Post date")
-    body = MarkdownField()
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ])
+
 
 BlogPost.content_panels = [
     FieldPanel("title", classname="full title"),
     FieldPanel('date'),
-    MarkdownPanel("body"),
+    StreamFieldPanel('body'),
 ]
