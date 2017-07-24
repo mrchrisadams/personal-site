559ff210be6ae373b3659a311bc5e1f537a3b0c2
Chris Adams <wave@chrisadams.me.uk>
2017-07-24 12:39:49 +0200

    Swap out Django, in favour of Flask
    I blog at wordpress.com - Falsk is just here for experiments, and might
    be replaced with something static anyway

--- home/wagtail_hooks.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.utils.html import format_html
-
-from wagtail.wagtailcore import hooks
-
-@hooks.register('insert_editor_css')
-def editor_css():
-    return format_html(
-        '<link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css ">')
