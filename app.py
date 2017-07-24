a170b885777732e0d02b575372634a6a3f568ee0
Chris Adams <wave@chrisadams.me.uk>
2017-07-24 13:00:23 +0200

    Add the flask-common
    This wraps whitenoise, flask and so on

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_common import Common
 
 app = Flask(__name__)
+common = Common(app)
 
 @app.route("/")
 def index():
     return render_template('home_page.html')
 
 if __name__ == "__main__":
-    app.run()
+    common.serve()
